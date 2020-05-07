f32a987c8ba526e102a21c22089124b245364119
Marco Alvarado <marcotrabajos2013@gmail.com>
2020-05-06 18:45:45 -0600

    Update Category.php

--- api-rest-blog/app/Category.php
+++ api-rest-blog/app/Category.php
@@ -7,8 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     //
-    protected $table='categories';
-    public function posts(){
-        return $this->hasMany('App\Post');
-    }
+    protected $table='category';
+
 }
