8911c12180abc58205ae8f12cfe9a0fec24d06d3
Juan <juanruizh2406@gmail.com>
2020-05-14 00:48:49 -0600

    name de rutas

--- api-rest-blog/routes/web.php
+++ api-rest-blog/routes/web.php
@@ -2,21 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-//Route::resource('/api/customer', 'CustomerController');
-//Route::get('/prueba','PruebaController@prueba');
-
-
 //especificas
-Route::post('/api/user/upload','UserController@upload');
-Route::get('/api/user/avatar/{filename}','UserController@avatar');
-Route::post('/api/user/login','UserController@login');
-Route::get('/api/user/getidentity','UserController@getIdentity');
+Route::post('/api/user/upload','UserController@upload')->name('user.upload');
+Route::get('/api/user/avatar/{filename}','UserController@avatar')->name('user.avatar');
+Route::post('/api/user/login','UserController@login')->name('user.login');
+Route::get('/api/user/getidentity','UserController@getIdentity')->name('user.getIdentity');
 //Route::get('/api/user/getidentity','UserController@getIdentity')->middleware(ApiAuthMiddleware::class);
 //automaticas RESTful
 Route::resource('/api/category', 'CategoryController');
 Route::resource('/api/customer', 'CustomerController');
 Route::resource('/api/product','ProductController');
-Route::resource('/api/sale_product','SaleProductController');
+Route::resource('/api/sale_product','Sale_ProductController');
 Route::resource('/api/sale','SaleController');
 Route::resource('/api/supplier','SupplierController');
 Route::resource('/api/user', 'UserController',['except' => ['create','edit']]);
